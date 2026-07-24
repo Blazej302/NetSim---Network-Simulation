20d06269c9b88e1bca05efa1cc47c970da13e1ee
Blazej302 <blaze117cnx@gmail.com>
2026-07-24 14:02:07 +0200

    completation of receiving system with class

--- include/receiver_types.hpp
+++ include/receiver_types.hpp
@@ -7,11 +7,26 @@
 
 #include <map>
 #include <memory>
+#include <optional>
 
 #include "types.hpp"
 #include "helpers.hpp"
+#include "Package.hpp"
+#include "storage_types.hpp"
 
-class IPackageReceiver;
+class IPackageReceiver
+{
+public:
+    virtual void receive_package(Package&& p) = 0;
+    virtual ElementID get_id() const = 0;
+
+    virtual IPackageStockpile::const_iterator begin() const = 0;
+    virtual IPackageStockpile::const_iterator end() const = 0;
+    virtual IPackageStockpile::const_iterator cbegin() const = 0;
+    virtual IPackageStockpile::const_iterator cend() const = 0;
+
+    virtual ~IPackageReceiver() = default;
+};
 
 class ReceiverPreferences
 {
@@ -38,6 +53,29 @@ private:
     ProbabilityGenerator pg_;
 };
 
+class PackageSender
+{
+private:
+    std::optional<Package> sending_buffer_ = std::nullopt;
+public:
+    ReceiverPreferences receiver_preferences_;
 
+    PackageSender() = default;
+
+    PackageSender(PackageSender&&) = default;
+    PackageSender& operator = (PackageSender&&) = default;
+
+    PackageSender(const PackageSender&) = delete;
+    PackageSender& operator = (const PackageSender&) = delete;
+
+    virtual ~PackageSender();
+
+    void push_package(Package&& p);
+    void send_package();
+
+    const std::optional<Package>& get_sending_buffer() const {return sending_buffer_;}
+protected:
+    void reset_buffer() {sending_buffer_.reset();}
+};
 
 #endif //NETSIM_RECEIVER_TYPES_HPP
--- src/receiver_types.cpp
+++ src/receiver_types.cpp
@@ -3,10 +3,92 @@
 //
 
 #include "receiver_types.hpp"
-
-#include <stdexcept>
+#include "receiver_types.hpp"
 
 void ReceiverPreferences::add_receiver(IPackageReceiver* r)
 {
-    
+    if (preferences_.empty())
+    {
+        preferences_[r] = 1.0;
+    }
+
+    if (preferences_.contains(r))
+    {
+        return;
+    }
+
+    preferences_[r] = 0.0;
+
+    double new_probability = 1.0 / static_cast<double>(preferences_.size());
+    for (auto& [receiver, probability] : preferences_)
+    {
+        probability = new_probability;
+    }
+
+}
+
+void ReceiverPreferences::remove_receiver(IPackageReceiver* r)
+{
+    if (!preferences_.contains(r))
+    {
+        return;
+    }
+
+    preferences_.erase(r);
+
+    if (preferences_.empty())
+    {
+        return;
+    }
+
+    double new_probability = 1.0 / static_cast<double>(preferences_.size());
+
+    for (auto& [receiver, probability]: preferences_)
+    {
+        probability = new_probability;
+    }
+}
+
+IPackageReceiver* ReceiverPreferences::choose_receiver()
+{
+    if (preferences_.empty())
+    {
+        return nullptr;
+    }
+
+    double p = pg_();
+    // we use distribution function to choose receiver
+    double cumulative_probability = 0.0;
+    for (const auto& [receiver, probability]:preferences_)
+    {
+        cumulative_probability += probability;
+        if (p <= cumulative_probability)
+        {
+            return receiver;
+        }
+    }
+
+    return preferences_.rbegin()->first;
+}
+
+void PackageSender::push_package(Package&& p)
+{
+    sending_buffer_ = std::move(p);
+}
+
+void PackageSender::send_package()
+{
+    if (!sending_buffer_.has_value())
+    {
+        return;
+    }
+
+    IPackageReceiver* receiver = receiver_preferences_.choose_receiver();
+
+    if (receiver != nullptr)
+    {
+        receiver->receive_package(std::move(sending_buffer_.value())); //std::move(*sending_buffer_)
+        sending_buffer_.reset();
+    }
+
 }
